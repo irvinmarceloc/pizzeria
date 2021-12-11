213be38451ddf574dada0de78fe6d2826bb8eba8
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-11 01:38:37 -0300

    Nombre singular para el modelo

--- producto/admin.py
+++ producto/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from producto.models import *
 
 ##Para que el usuario pueda administrar las tablas desde el panel
-class PizzasAdmin(admin.ModelAdmin):
+
+#Administrar Pizzas
+class PizzaAdmin(admin.ModelAdmin):
     list_display=("nombre", "precio", "activo") #Para ver con formatos
     search_fields=("nombre","activo") #Para filtrar por buscador
+#Administrar Ingredientes
+class IngredienteAdmin(admin.ModelAdmin):
+    list_display=("nombre", "categoria") #Para ver con formatos
+    nombre=models.CharField(max_length=30)
+    categoria=models.BooleanField()
 
-admin.site.register(Pizzas, PizzasAdmin)
+
+admin.site.register(Pizza, PizzaAdmin)
+admin.site.register(Ingrediente, IngredienteAdmin)
+
--- producto/models.py
+++ producto/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 #Modelo de piza
-class Pizzas(models.Model):
+class Pizza(models.Model):
     nombre=models.CharField(max_length=30)
     precio=models.IntegerField()
     activo=models.BooleanField()
 
 #Modelo Ingrediente
-class Ingredientes(models.Model):
+class Ingrediente(models.Model):
     nombre=models.CharField(max_length=30)
     categoria=models.BooleanField()
 
