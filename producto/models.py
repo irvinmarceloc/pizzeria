0c85d0f266ae99c8376b80c1564c34c9dc29c01f
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-11 01:09:52 -0300

    Modelo creado para pizza e ingrediente

--- producto/models.py
+++ producto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+#Modelo de piza
+class Pizzas(models.Model):
+    nombre=models.CharField(max_length=30)
+    precio=models.IntegerField()
+    activo=models.BooleanField()
+
+#Modelo Ingrediente
+class Ingredientes(models.Model):
+    nombre=models.CharField(max_length=30)
+    categoria=models.BooleanField()
+
+
