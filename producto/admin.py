f23399625e18426f0ecb9ad2b22fcc9718b08ca2
irvinmarceloc <irvinmarceloc@gmail.com>
2021-12-11 01:29:13 -0300

    Habilitar administracion de tabla PIZZAS

--- producto/admin.py
+++ producto/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-
+from producto.models import *
 
 ##Para que el usuario pueda administrar las tablas desde el panel
 class PizzasAdmin(admin.ModelAdmin):
     list_display=("nombre", "precio", "activo") #Para ver con formatos
     search_fields=("nombre","activo") #Para filtrar por buscador
+
+admin.site.register(Pizzas, PizzasAdmin)
